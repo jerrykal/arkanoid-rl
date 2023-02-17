c96407535ffa17042041645698e4336318b20229
Jerry Lee <oojerryleeoo@gmail.com>
2023-02-17 20:49:54 +0800

    Implement Dueling-DQN network architecture

--- ml/model.py
+++ ml/model.py
@@ -1,13 +1,23 @@
-import torch
 import torch.nn as nn
 
 
+# Dueling DQN
 class QNet(nn.Module):
     def __init__(self, state_size, n_actions):
         super().__init__()
         self.fc = nn.Sequential(
-            nn.Linear(state_size, 512), nn.ReLU(), nn.Linear(512, n_actions)
+            nn.Linear(state_size, 512),
+            nn.ReLU(),
+        )
+        self.fc_value = nn.Sequential(
+            nn.Linear(512, 1),
+        )
+        self.fc_advantage = nn.Sequential(
+            nn.Linear(512, n_actions),
         )
 
     def forward(self, state):
-        return self.fc(state)
+        fc_out = self.fc(state)
+        value = self.fc_value(fc_out)
+        advantage = self.fc_advantage(fc_out)
+        return value + advantage - advantage.mean(dim=-1, keepdim=True)
